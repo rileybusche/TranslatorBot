51ecdfe1cee13048a673dcf631a70752db5470ee
Riley Busche <riley.busche@gmail.com>
2021-03-30 19:31:34 -0500

    Adding Presence

--- bot.py
+++ bot.py
@@ -44,6 +44,8 @@ def store_english_translation(english_text):
 @client.event
 async def on_message(message):
 
+    print(message.content)
+
     author = str(message.author)
     channel = message.channel
     msg =  message.content.strip().lower()
